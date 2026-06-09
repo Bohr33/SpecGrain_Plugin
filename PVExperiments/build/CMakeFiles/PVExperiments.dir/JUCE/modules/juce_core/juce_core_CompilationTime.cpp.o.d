7a3d0fc661a833d2a85d9c8b0b0ade6bdd557af5
Ben Ward <brw775586@gmail.com>
2026-06-09 00:20:13 -0400

    removed tracking on build folder, add to .gitignore

--- PVExperiments/build/CMakeFiles/PVExperiments.dir/JUCE/modules/juce_core/juce_core_CompilationTime.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/PVExperiments.dir/JUCE/modules/juce_core/juce_core_CompilationTime.cpp.o: \
-  /Users/benward_1/repos/Plugins/PVExperiments/PVExperiments/JUCE/modules/juce_core/juce_core_CompilationTime.cpp
--- PVExperiments/build/CMakeFiles/PVExperiments.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "PVExperiments_artefacts/libPVExperiments_SharedCode.a"
-)
